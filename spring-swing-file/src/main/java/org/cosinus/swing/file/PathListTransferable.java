1e49e2c27adf1f051e6dc5c2901171399df06ce7
Cosinus Code <cosinus.ro@gmail.com>
2026-03-22 09:23:22 +0200

    add viewId in PathListTransferable

--- spring-swing-file/src/main/java/org/cosinus/swing/file/PathListTransferable.java
+++ spring-swing-file/src/main/java/org/cosinus/swing/file/PathListTransferable.java
@@ -88,6 +88,10 @@ public class PathListTransferable implements Transferable {
         @Setter
         private boolean moveTransfer;
 
+        @Getter
+        @Setter
+        private String viewId;
+
         PathListTransferData(final List<Path> paths) {
             super(paths);
         }
